79d5e17720d423d18e51156a35b9f1485e79b61e
Kong5109 <kong8071.kk@gmail.com>
2026-04-30 20:47:13 +0700

    Shooter Scripts

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] private Transform shootPoint;
+    [SerializeField] private GameObject target;
+    [SerializeField] private GameObject bulletPrefab;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 screenPos = Mouse.current.position.ReadValue();
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(screenPos);
+            Debug.DrawRay(ray.origin, ray.direction * 5f, Color.red, 5f);
+
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+
+            if (hit.collider != null)
+            {
+                target.transform.position = new Vector2(hit.point.x, hit.point.y);
+                Debug.Log($"Hit{hit.collider.gameObject.name}");
+
+                Vector2 projectileVelocity = CalculateProjectileVelocity(shootPoint.position, hit.point, 1f);
+
+                Rigidbody2D shootBullet = Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity).GetComponent<Rigidbody2D>();
+
+                shootBullet.linearVelocity = projectileVelocity;
+            }
+        }
+    }
+
+    Vector2 CalculateProjectileVelocity(Vector2 origin,Vector2 target,float time)
+    {
+        Vector2 direction = target - origin;
+        return new Vector2(direction.x / time, direction.y / time + 05f * Mathf.Abs(Physics2D.gravity.y) * time);
+    }
+}
